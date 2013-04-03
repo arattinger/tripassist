5d6a890c7f5c3a7a6e8def9813ecb57e4f8e5b04
Andre Rattinger <andre.rattinger@gmail.com>
2013-04-03 11:57:23 +0200

    basic template setup

--- website/website/urls.py
+++ website/website/urls.py
@@ -4,10 +4,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
-    # url(r'^website/', include('website.foo.urls')),
+urlpatterns = patterns(
+    '',
+    url(r'^mobile/', include('mobile.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
