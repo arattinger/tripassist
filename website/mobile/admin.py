0a2cbad6788a2e6810229a682e3e531d6cc9a830
Andre Rattinger <andre.rattinger@gmail.com>
2013-04-04 22:56:21 +0200

    model update for attachment uploads + description fields

---
+++ website/mobile/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from models import Accomodation, Holiday, Place, Route, Attachment
+
+admin.site.register(Accomodation)
+admin.site.register(Holiday)
+admin.site.register(Place)
+admin.site.register(Route)
+admin.site.register(Attachment)
--- website/mobile/models.py
+++ website/mobile/models.py
@@ -1,14 +1,19 @@
 from django.db import models
 from django.utils.timezone import now
+from django.contrib.auth.models import User
+from os.path import join
 
 
 def _get_upload_dir(instance, filename):
-    pass
+    upload_dir = join("attachment", instance.user.username, filename)
+    return upload_dir
 
 
 class Attachment(models.Model):
     attachment = models.FileField(upload_to=_get_upload_dir, blank=True,
                                   max_length=512)
+    # Save the user in the attachment model for easier lookups
+    user = models.ForeignKey(User)
 
 
 class Route(models.Model):
@@ -30,8 +35,11 @@ class Route(models.Model):
     arrival_latitude = models.FloatField(null=True, blank=True)
     arrival_altitude = models.FloatField(null=True, blank=True)
     arrival_address = models.CharField(max_length=255, blank=True)
-
+    description = models.TextField(blank=True)
     files = models.ManyToManyField(Attachment, blank=True)
+
+    def __unicode__(self):
+        return u"%s: %s" % (self.name, self.type)
 
 
 class Place(models.Model):
@@ -47,10 +55,15 @@ class Place(models.Model):
     phone_number = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=255, blank=True)
     files = models.ManyToManyField(Attachment, blank=True)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return u"%s: %s" % (self.name, self.type)
 
 
 class Accomodation(models.Model):
     name = models.CharField(max_length=255, blank=True)
+    type = models.PositiveSmallIntegerField()
     created = models.DateTimeField(default=now)
     last_changed = models.DateTimeField(default=now)
     longitude = models.FloatField(null=True, blank=True)
@@ -61,9 +74,12 @@ class Accomodation(models.Model):
     phone_number = models.CharField(max_length=50, blank=True)
     address = models.CharField(max_length=255, blank=True)
     files = models.ManyToManyField(Attachment, blank=True)
+    description = models.TextField(blank=True)
+    start = models.DateField(null=True, blank=True)
+    end = models.DateField(null=True, blank=True)
 
-    start = models.DateTimeField(null=True, blank=True)
-    end = models.DateTimeField(null=True, blank=True)
+    def __unicode__(self):
+        return u"%s: %s" % (self.name, self.type)
 
 
 class Holiday(models.Model):
@@ -73,3 +89,8 @@ class Holiday(models.Model):
     accomodations = models.ManyToManyField(Accomodation, blank=True)
     places = models.ManyToManyField(Place, blank=True)
     routes = models.ManyToManyField(Route, blank=True)
+    description = models.TextField(blank=True)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return u"%s" % (self.name)
