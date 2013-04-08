0a6545a8da49f8c1fdcc010bdf7898d9acf30b0c
Paul Laufer <paul@laufer.at>
2013-04-08 12:19:50 +0200

    added empty mocha testing framework

---
+++ website/mobile/static/js/specs/boot.js
@@ -0,0 +1,61 @@
+$(document).ready(function() {
+
+    // define all classes to be fetched from server
+    var CLASSES = [
+        'application',
+        'views/selectholidayview'
+    ];
+
+    // define all templates to be fetched from server
+    var TEMPLATES = [
+        'main',
+        'selectholidayview'
+    ];
+
+    var loadClassCounter = 0;
+    var loadTemplatesCounter = 0;
+
+    function callback() {
+        if (loadClassCounter == CLASSES.length * 2 && loadTemplatesCounter == TEMPLATES.length) {
+
+            mocha.run();
+        }
+    }
+
+    // load them and start tests when done
+    for (var i = 0; i<CLASSES.length; i++) {
+        $.getScript('/static/js/' + CLASSES[i] + '.js').done(function() {
+            loadClassCounter++;
+            callback();
+        }).fail(function(jqxhr, settings, exception) {
+            console.log(exception);
+        })
+    }
+
+    // load tests
+    for (var i = 0; i<CLASSES.length; i++) {
+        $.getScript('/static/js/specs/specs/' + CLASSES[i] + '.spec.js').done(function() {
+            loadClassCounter++;
+            callback();
+        }).fail(function(jqxhr, settings, exception) {
+            console.log(exception);
+        })
+    }
+
+    for (var i = 0; i<TEMPLATES.length; i++) {
+        $.get('/static/js/templates/' + TEMPLATES[i] + '.template', null, null, 'html').done(function(template, textStatus, jqxhr) {
+            loadTemplatesCounter++;
+
+            var names = this.url.split('/');
+            TemplateManager.addTemplate(names[names.length-1], template);
+
+            callback();
+
+        }).fail(function(jqxhr, settings, exception) {
+            console.log(exception);
+        })
+    }
+
+    // setup mocha
+    mocha.setup('bdd');
+});
---
+++ website/mobile/static/js/specs/specs/views/selectholidayview.spec.js
@@ -0,0 +1,3 @@
+describe('SelectHolidayView', function() {
+
+});
