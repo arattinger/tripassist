f375bd42eb35ea5f229802b6d59c82afc9cbd313
Paul Laufer <paul@laufer.at>
2013-04-08 12:23:09 +0200

    added basic tests for application

--- website/mobile/static/js/specs/specs/application.spec.js
+++ website/mobile/static/js/specs/specs/application.spec.js
@@ -5,4 +5,15 @@ describe('Application', function() {
         app = new TripAssist.Application();
     });
 
+    it('main container filled', function() {
+        expect ( $('#main-ctn').html().length ).to.be(0);
+        app.start();
+        expect ( $('#main-ctn').html().length ).not.to.be(0);
+    });
+
+    it('selectholidayview loaded initially', function() {
+        app.start();
+        expect ( $('#title').text() ).to.be('Select Holiday');
+    });
+
 });
