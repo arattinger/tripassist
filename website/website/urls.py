8cd9fe88bb8b1c3455fa01d593a4997bde6f5b6b
Andre Rattinger <andre.rattinger@gmail.com>
2013-06-17 20:41:33 +0200

    new urls for the api login

--- website/website/urls.py
+++ website/website/urls.py
@@ -12,6 +12,8 @@ urlpatterns = patterns(
         {'template_name': 'login.html'}),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
         {'next_page': '/holiday/'}, name='auth_logout'),
+
+    (r'^accounts/api_login/$', 'website.backends.login_user'),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
